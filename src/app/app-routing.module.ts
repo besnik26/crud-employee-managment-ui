94cf55cfb9f8aba88f87b65a4e330c300017355a
Besnik Beka <besnikbeka2001@gmail.com>
2025-05-13 10:25:03 +0200

    Setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'header', component:HeaderComponent},
+  {path: '', component:HomeComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

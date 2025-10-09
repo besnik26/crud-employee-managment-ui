f709c230d3332031d9531bd6a08168f4c165c2fa
Besnik Beka <besnikbeka2001@gmail.com>
2025-10-09 16:41:46 +0200

    Update

--- src/app/shared/side-panel/side-panel.component.ts
+++ src/app/shared/side-panel/side-panel.component.ts
@@ -1,24 +1,29 @@
-import { Component, ElementRef, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PanelService } from 'src/app/services/panel.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { CompanyWithUsersDto } from 'src/app/models/CompanyWithUsersDto';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-side-panel',
   templateUrl: './side-panel.component.html',
   styleUrls: ['./side-panel.component.scss']
 })
-export class SidePanelComponent {
+export class SidePanelComponent implements OnInit{
   sidePanelOpen?:boolean;
+  companies: CompanyWithUsersDto[] = [];
+  memberDropdownOpen: boolean = false;
+  newsDropdownOpen: boolean = false;
 
   constructor(
     private panelService:PanelService,
     private router:Router,
     public authService:AuthService,
     private toaster:ToastrService,
-    private elementRef:ElementRef
-
+    private elementRef:ElementRef,
+    private adminService:AdminService
   ){ 
     this.panelService.sidePanelState$.subscribe(state => {
         this.sidePanelOpen = state;
@@ -26,10 +31,24 @@ export class SidePanelComponent {
 
   }
 
+  ngOnInit(): void {
+    this.adminService.getMyCompanies().subscribe(data => this.companies = data);
+  }
+
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
     this.toaster.info('Logged out successfully!')
+  }
+
+  toggleMemberDropdown(): void {
+    this.memberDropdownOpen = !this.memberDropdownOpen;
+    this.newsDropdownOpen = false;
+  }
+
+  toggleNewsDropdown(): void {
+    this.newsDropdownOpen = !this.newsDropdownOpen;
+    this.memberDropdownOpen = false;
   }
 
   // @HostListener('document:click', ['$event'])

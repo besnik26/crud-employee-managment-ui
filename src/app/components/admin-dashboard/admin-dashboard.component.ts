3467eb81096d3ee6c6f38a667a34229c4bea444a
besnik <besnikbeka2001@gmail.com>
2025-12-22 15:05:54 +0100

    update

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -4,6 +4,7 @@ import { AdminService } from 'src/app/services/admin.service';
 import { AdminDashboardDto } from 'src/app/models/admin-dashboard-dto.model';
 import { CompanyWithUsersDto } from 'src/app/models/CompanyWithUsersDto';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
@@ -12,26 +13,40 @@ import { Router } from '@angular/router';
 export class AdminDashboardComponent implements OnInit{
   dashboardData?: AdminDashboardDto;
   companies: CompanyWithUsersDto[] = [];
+  totalEmployees = 0;
 
-  constructor(private adminService: AdminService, public router:Router) {}
+  constructor(
+    private adminService: AdminService, 
+    public router:Router,
+    private toaster:ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.adminService.getDashboard().subscribe(data => this.dashboardData = data);
-    this.adminService.getMyCompanies().subscribe(data => this.companies = data);
+    this.adminService.getMyCompanies().subscribe(data => {
+      this.companies = data
+      this.calculateTotalEmployees(data);
+    });
   }
 
 
+  calculateTotalEmployees(companies: any[]): void {
+    this.totalEmployees = companies.reduce((total, company) => {
+      return total + company.users.length;
+    }, 0);
+  }
 
   deleteCompany(companyId: number): void {
     if (confirm('Are you sure you want to delete this company?')) {
       this.adminService.deleteCompany(companyId).subscribe({
         next: () => {
           this.ngOnInit();
-          alert('Company deleted successfully.');
+          this.toaster.success('Company deleted successfully!')
+
         },
         error: (err) => {
           console.error(err);
-          alert('Failed to delete company.');
+          this.toaster.error('Failed to delete company!')
         }
       });
     }
